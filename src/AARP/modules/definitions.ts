75a16798524eef48fc71700ce4840a82745da259
null <null>
2026-01-07 22:42:14 -0500

    feat(AARP): get and display activities list

--- src/AARP/modules/definitions.ts
+++ src/AARP/modules/definitions.ts
@@ -1,4 +1,15 @@
+import * as z from "zod";
 import { createMessage, createTabMessage } from "../../modules/hermes";
+
+export class NotLoggedInError extends Error {
+  name: string = "NotLoggedInError";
+  loginUrl: string;
+
+  constructor(message: string, loginUrl: string) {
+    super(message);
+    this.loginUrl = loginUrl;
+  }
+}
 
 // Types and schemas -------------------
 
@@ -8,15 +19,45 @@ export interface AarpUser {
   accessToken: string;
 }
 
-// Content script message definitions -------------------
+// TODO: look here for styling and cool options (like searching for topics, for example)
+const ActivitySchema = z.object({
+  identifier: z.uuid(),
+  startDate: z.iso.datetime(),
+  endDate: z.iso.datetime(),
+  activityType: z.object({
+    identifier: z.string(),
+    basePointValue: z.number(),
+    name: z.string(),
+    visibleOnSite: z.boolean(),
+    active: z.nullable(z.boolean()),
+  }),
+  name: z.string(),
+  category: z.string(),
+  url: z.union([z.url(), z.string().length(0)]),
+  imageUrl: z.union([z.url(), z.string().length(0), z.null()]),
+  description: z.nullable(z.string()),
+  primaryTopic: z.nullable(z.string()),
+  active: z.boolean(),
+  deleted: z.nullable(z.boolean()),
+  membersOnly: z.boolean(),
+});
+export type AarpActivity = z.infer<typeof ActivitySchema>;
+export const ActivitiesListSchema = z.array(ActivitySchema);
+
+// Content script message definitions -------------------------------------------------------------
 
 export const [getTabLocalStorage, onTabLocalStorageRequest] = createTabMessage<
   string,
   string | null
 >("getTabLocalStorage");
 
-// Service worker message definitions -------------------
+// Service worker message definitions -------------------------------------------------------------
 
 export const [getUser, onGetUserRequest] = createMessage<void, AarpUser | null>(
   "getAarpUser"
 );
+
+export const [getActivities, onActivitiesRequest] = createMessage<
+  number,
+  AarpActivity[]
+>("getAarpActivites");
