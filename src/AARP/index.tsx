80485c417372ae9a55fdbd3cb18f283aaaba1806
anonymous <anonymous>
2026-01-09 07:04:07 -0500

    feat(AARP): shown activities mechanism / data hooks (with filtering)
    Placeholder implementation of activity filtering for fleshing out later.

--- src/AARP/index.tsx
+++ src/AARP/index.tsx
@@ -10,12 +10,17 @@ import {
   AarpUser,
   earnActivityRewards,
   getActivities,
+  getActivityStatuses,
   getUser,
   updateAarpTab,
 } from "./modules/definitions";
 import LoadingAnimation from "../components/LoadingAnimation";
 import { isAarpTab, LOGIN_URL } from "./modules/tools";
 import { simpleDeepCompare } from "../modules/utils";
+import {
+  ActivitiesFilter,
+  applyActivitiesFilter,
+} from "./modules/activitiesFilter";
 
 type AARPData =
   | {
@@ -24,6 +29,7 @@ type AARPData =
       activities: AarpActivity[];
     }
   | { status: "notLoggedIn" };
+type ActivityStatus = "complete" | "incomplete" | "unknown";
 
 const MAX_DAILY_REWARDS = 5000;
 const ACTIVITIES_CHUNK_SIZE = 5;
@@ -33,10 +39,10 @@ const AARPDataContext = createContext<AARPData | "loading">("loading");
 
 function Activity({
   activityIdx,
-  isCompleted,
+  status,
 }: {
   activityIdx: number;
-  isCompleted: boolean;
+  status: ActivityStatus;
 }) {
   const aarpData = useContext(AARPDataContext);
 
@@ -52,14 +58,16 @@ function Activity({
     <div>
       <div>
         <h4>{activity.name}</h4>
-        {isCompleted ? (
+        {status === "complete" ? (
           <p>Completed</p>
-        ) : (
+        ) : status === "incomplete" ? (
           <p>{activity.activityType.basePointValue}</p>
+        ) : (
+          <p>Error fetching status</p>
         )}
       </div>
       <p>{activity.description}</p>
-      {isCompleted || (
+      {status === "incomplete" && (
         <a
           onClick={() =>
             earnActivityRewards({
@@ -78,9 +86,8 @@ function Activity({
 
 function AARP() {
   const aarpData = useContext(AARPDataContext);
-  const [nActivitiesDisplayed, setNActivitiesDisplayed] = useState<number>(
-    ACTIVITIES_CHUNK_SIZE
-  );
+
+  // Handle edge cases (loading or not logged in) -------------------------------------------------
 
   if (aarpData === "loading") return <LoadingAnimation />;
   if (aarpData.status !== "loggedIn")
@@ -97,6 +104,84 @@ function AARP() {
       </div>
     );
 
+  // Filtering, displaying, and fetching statuses of activities -----------------------------------
+
+  const [activitiesAreLoading, setActivitiesAreLoading] =
+    useState<boolean>(true);
+  const [activitiesFilter, setActivitiesFilter] = useState<ActivitiesFilter>(
+    {}
+  );
+  const [filteredActivities, setFilteredActivities] = useState<number[]>([]);
+  const [activityStatuses, setActivityStatuses] = useState<{
+    [key: number]: ActivityStatus;
+  }>({});
+  const [nActivitiesShown, setNActivitiesShown] = useState<number>(
+    ACTIVITIES_CHUNK_SIZE
+  );
+
+  // Hook for applying filters
+  useEffect(() => {
+    if (aarpData.activities.length > 0 && nActivitiesShown > 0) {
+      setActivitiesAreLoading(true);
+
+      // Async because I want the activitiesAreLoading to be applied
+      new Promise<ReturnType<typeof applyActivitiesFilter>>((resolve) =>
+        resolve(applyActivitiesFilter(aarpData.activities, activitiesFilter))
+      ).then((newFilteredActivities) => {
+        setFilteredActivities(newFilteredActivities);
+        setActivitiesAreLoading(false);
+      });
+    }
+  }, [activitiesFilter, aarpData.activities, aarpData.user]);
+
+  useEffect(() => {
+    if (filteredActivities.length > 0 && nActivitiesShown > 0) {
+      const activitiesToCheck = filteredActivities
+        .slice(0, nActivitiesShown) // Don't worry about those not shown
+        .filter((activityIdx) => activityStatuses[activityIdx] !== "complete")
+        .map((activityIdx) => ({
+          idx: activityIdx,
+          id: aarpData.activities[activityIdx].identifier,
+        }));
+
+      if (activitiesToCheck.length > 0) {
+        getActivityStatuses({
+          activityIds: activitiesToCheck.map(({ id }) => id),
+          userFedId: aarpData.user.fedId,
+          accessToken: aarpData.user.accessToken,
+        })
+          .then(({ activityFinishedStatuses, userDailyPointsLeft }) => {
+            // TODO: activity status response also gives the daily rewards left, which should be used to update it whenever we get there
+            const newActivityStatuses = activityFinishedStatuses.reduce(
+              (activityStatuses, isComplete, checkIdx) => {
+                activityStatuses[activitiesToCheck[checkIdx].idx] =
+                  isComplete === undefined
+                    ? "unknown"
+                    : isComplete
+                    ? "complete"
+                    : "incomplete";
+                return activityStatuses;
+              },
+              { ...activityStatuses }
+            );
+            setActivityStatuses(newActivityStatuses);
+          })
+          .catch((reason) => {
+            console.error(
+              "Failed to fetch statuses for activities:",
+              reason,
+              "(activities:",
+              activitiesToCheck,
+              ")"
+            );
+            setActivityStatuses([]);
+          });
+      }
+    }
+  }, [filteredActivities, nActivitiesShown]);
+
+  // The UI and stuff -----------------------------------------------------------------------------
+
   return (
     <div>
       <div>
@@ -105,14 +190,40 @@ function AARP() {
         <h3>Daily points left: {aarpData.user.dailyPointsLeft ?? "unknown"}</h3>
       </div>
       <div>
-        {aarpData.activities.length > 0 ? (
+        {activitiesAreLoading ? (
+          <LoadingAnimation />
+        ) : aarpData.activities.length > 0 ? (
           <>
             <div>
-              {aarpData.activities
-                .slice(0, nActivitiesDisplayed)
-                .map((_, idx) => (
-                  <Activity key={idx} activityIdx={idx} isCompleted={false} />
-                ))}
+              {nActivitiesShown > 0 ? (
+                filteredActivities
+                  .slice(0, nActivitiesShown)
+                  .map((activityIdx) => (
+                    <Activity
+                      key={activityIdx}
+                      activityIdx={activityIdx}
+                      status={activityStatuses[activityIdx] ?? "unknown"}
+                    />
+                  ))
+              ) : (
+                <p>Showing 0 activities</p>
+              )}
+            </div>
+            <div>
+              <button
+                onClick={() =>
+                  setNActivitiesShown(nActivitiesShown - ACTIVITIES_CHUNK_SIZE)
+                }
+              >
+                Show less
+              </button>
+              <button
+                onClick={() =>
+                  setNActivitiesShown(nActivitiesShown + ACTIVITIES_CHUNK_SIZE)
+                }
+              >
+                Show more
+              </button>
             </div>
           </>
         ) : (
