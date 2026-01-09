8c3be26fa178743b8b35359e060dd06e211eb8c9
anonymous <anonymous>
2026-01-09 07:07:10 -0500

    feat(AARP): on update to login cookies/storage, send updated user to side panel

--- src/AARP/content-script.ts
+++ src/AARP/content-script.ts
@@ -1,5 +1,19 @@
-import { onTabLocalStorageRequest } from "./modules/definitions";
+import {
+  alertPossibleUserChange,
+  onTabLocalStorageRequest,
+  USER_STORAGE_KEYS,
+} from "./modules/definitions";
 
 onTabLocalStorageRequest(async (sendResponse, key) =>
   sendResponse(localStorage.getItem(key))
 );
+
+// Add listeners for localStorage to alert the service worker that the user may have changed
+
+window.addEventListener("storage", (event) => {
+  if (
+    event.storageArea === localStorage &&
+    USER_STORAGE_KEYS.includes(event.key!)
+  )
+    alertPossibleUserChange();
+});
